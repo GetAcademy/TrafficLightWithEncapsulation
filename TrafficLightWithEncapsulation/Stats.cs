640bfffbaf80bf0cd20c8c6615843e1bd5d75370
GET Academy <terje@kolderup.net>
2024-11-28 12:21:13 +0100

    x

---
+++ TrafficLightWithEncapsulation/Stats.cs
@@ -0,0 +1,20 @@
+﻿namespace TrafficLightWithEncapsulation
+{
+    internal class Stats
+    {
+        private int _sum;
+        private int _n;
+
+        public void AddNumber(int number)
+        {
+            _n++;
+            _sum += number;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Sum: {_sum} Snitt: {_sum/_n} Ant tall: {_n}");
+
+        }
+    }
+}
